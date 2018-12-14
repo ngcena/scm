a0173fd1c11f00a3b61beadf2f4c2d214a1b789e
dbelamate <dbelamate@frvm.utn.edu.ar>
2018-12-14 12:28:39 -0300

    pedidoComercioAdherido
    Actualización de metodo calcularTotal

--- PedidoComercioAdherido.java
+++ PedidoComercioAdherido.java
@@ -1,1 +1,15 @@
 import java.math.BigDecimal;
+import java.util.Iteracto;
+import java.util.List;
+
+public class PedidoComercioAdherido {
+    private List<DetallePedido> detalle;
+
+    public PedidoComercioAdherido() {
+        this.detalle = new ArrayList<>();
+    }
+
+    public BigDecimal calcularTotal() {
+        return total;
+    }
+}
