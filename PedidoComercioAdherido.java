0cf9b27cd108025804df53e5e013dd9a1a47fab0
Norberto Gaspar Cena <ngcena@frvm.utn.edu.ar>
2018-12-14 12:48:44 -0300

    Actualización a ultima version

--- PedidoComercioAdherido.java
+++ PedidoComercioAdherido.java
@@ -1,21 +1,20 @@
 import java.math.BigDecimal;
-import java.util.Iteracto;
 import java.util.List;
 
-	for (DetallPedido detallePedido : detalle){
 public class PedidoComercioAdherido {
+
     private List<DetallePedido> detalle;
 
     public PedidoComercioAdherido() {
         this.detalle = new ArrayList<>();
-
     }
+    
     public BigDecimal calcularTotal() {
 	BigDecimal total=BigDecimal.ZERO;
-	
+
+	for (DetallPedido detallePedido : detalle){
 		total = total.add(detallePedido.calcularSubtotal());
 	}
-
         return total;
     }
 }
